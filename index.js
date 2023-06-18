86d7a5e67a07770b9ffd314ef3f6aa45034db6c4
SushantDahal <SushanDahal246@gmail.com>
2023-06-18 12:26:23 +0545

    change in range

--- index.js
+++ index.js
@@ -2,11 +2,23 @@ function myFunction() {
   var billTotal = document.getElementById("billTotal").value;
   var tipPercent = document.getElementById("tip-percent").value;
 
-  var tipAmount = (billTotal * tipPercent) / 100;
+  if (billTotal === "" || tipPercent === "") {
+    alert("Enter the Value in empty field!");
+  } else {
+    var tipAmount = (billTotal * tipPercent) / 100;
 
-  var totalBillWithTip = parseFloat(billTotal) + parseFloat(tipAmount);
+    var totalBillWithTip = parseFloat(billTotal) + parseFloat(tipAmount);
 
-  document.getElementById("tipAmount").value = tipAmount.toFixed(2);
-  document.getElementById("totalBillWithTip").value =
-    totalBillWithTip.toFixed(2);
+    document.getElementById("tipAmount").value = tipAmount.toFixed(2);
+    document.getElementById("totalBillWithTip").value =
+      totalBillWithTip.toFixed(2);
+  }
 }
+
+const tipRange = document.getElementById("tipRange");
+const tipPercentInput = document.getElementById("tip-percent");
+
+tipRange.addEventListener("input", function () {
+  tipPercentInput.value = tipRange.value;
+  myFunction();
+});
